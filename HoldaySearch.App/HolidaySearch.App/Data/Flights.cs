a3b57db2bb4e1286078225cb6ba8ab69a953cfba
MatthewWinterbottom <matthewwinterbottom@outlook.com>
2025-03-04 20:58:32 +0000

    Switch out data from JSON data and instead store in C# for simplification and compile-time safety

---
+++ HoldaySearch.App/HolidaySearch.App/Data/Flights.cs
@@ -0,0 +1,74 @@
+﻿namespace HolidaySearch.App.Data;
+
+/// <summary>
+/// I don't think deserialising JSON is something you're interested in
+/// So I'm going to simplify by loading into C#.
+/// This also gives us compile-time safety.
+/// Sorry if you were interested in JSON deserialisation :(
+/// </summary>
+public static class Flights
+{
+    public static List<Flight> Data =
+    [
+        new()
+        {
+            Id = 1, Airline = "First Class Air", From = "MAN", To = "TFS", Price = 470,
+            DepartureDate = new DateTime(2023, 7, 1)
+        },
+        new()
+        {
+            Id = 2, Airline = "Oceanic Airlines", From = "MAN", To = "AGP", Price = 245,
+            DepartureDate = new DateTime(2023, 7, 1)
+        },
+        new()
+        {
+            Id = 3, Airline = "Trans American Airlines", From = "MAN", To = "PMI", Price = 170,
+            DepartureDate = new DateTime(2023, 6, 15)
+        },
+        new()
+        {
+            Id = 4, Airline = "Trans American Airlines", From = "LTN", To = "PMI", Price = 153,
+            DepartureDate = new DateTime(2023, 6, 15)
+        },
+        new()
+        {
+            Id = 5, Airline = "Fresh Airways", From = "MAN", To = "PMI", Price = 130,
+            DepartureDate = new DateTime(2023, 6, 15)
+        },
+        new()
+        {
+            Id = 6, Airline = "Fresh Airways", From = "LGW", To = "PMI", Price = 75,
+            DepartureDate = new DateTime(2023, 6, 15)
+        },
+        new()
+        {
+            Id = 7, Airline = "Trans American Airlines", From = "MAN", To = "LPA", Price = 125,
+            DepartureDate = new DateTime(2022, 11, 10)
+        },
+        new()
+        {
+            Id = 8, Airline = "Fresh Airways", From = "MAN", To = "LPA", Price = 175,
+            DepartureDate = new DateTime(2023, 11, 10)
+        },
+        new()
+        {
+            Id = 9, Airline = "Fresh Airways", From = "MAN", To = "AGP", Price = 140,
+            DepartureDate = new DateTime(2023, 4, 11)
+        },
+        new()
+        {
+            Id = 10, Airline = "First Class Air", From = "LGW", To = "AGP", Price = 225,
+            DepartureDate = new DateTime(2023, 7, 1)
+        },
+        new()
+        {
+            Id = 11, Airline = "First Class Air", From = "LGW", To = "AGP", Price = 155,
+            DepartureDate = new DateTime(2023, 7, 1)
+        },
+        new()
+        {
+            Id = 12, Airline = "Trans American Airlines", From = "MAN", To = "AGP", Price = 202,
+            DepartureDate = new DateTime(2023, 10, 25)
+        }
+    ];
+}
---
+++ HoldaySearch.App/HolidaySearch.App/Data/Hotels.cs
@@ -0,0 +1,88 @@
+﻿namespace HolidaySearch.App.Data;
+
+/// <summary>
+/// See comment on <see cref="Flights"/> regarding why the JSOn is now C#
+/// </summary>
+public static class Hotels
+{
+    public static List<Hotel> Data =
+    [
+        new()
+        {
+            Id = 1, Name = "Iberostar Grand Portals Nous", ArrivalDate = new DateTime(2022, 11, 5), PricePerNight = 100,
+            LocalAirports = [ "TFS" ], Nights = 7
+        },
+
+        new()
+        {
+            Id = 2, Name = "Laguna Park 2", ArrivalDate = new DateTime(2022, 11, 5), PricePerNight = 50,
+            LocalAirports = [ "TFS" ], Nights = 7
+        },
+
+        new()
+        {
+            Id = 3, Name = "Sol Katmandu Park & Resort", ArrivalDate = new DateTime(2023, 6, 15), PricePerNight = 59,
+            LocalAirports = [ "PMI" ], Nights = 14
+        },
+
+        new()
+        {
+            Id = 4, Name = "Sol Katmandu Park & Resort", ArrivalDate = new DateTime(2023, 6, 15), PricePerNight = 59,
+            LocalAirports = [ "PMI" ], Nights = 14
+        },
+
+        new()
+        {
+            Id = 5, Name = "Sol Katmandu Park & Resort", ArrivalDate = new DateTime(2023, 6, 15), PricePerNight = 60,
+            LocalAirports = [ "PMI" ], Nights = 10
+        },
+
+        new()
+        {
+            Id = 6, Name = "Club Maspalomas Suites and Spa", ArrivalDate = new DateTime(2022, 11, 10),
+            PricePerNight = 75, LocalAirports = [ "LPA" ], Nights = 14
+        },
+
+        new()
+        {
+            Id = 7, Name = "Club Maspalomas Suites and Spa", ArrivalDate = new DateTime(2022, 9, 10),
+            PricePerNight = 76, LocalAirports = [ "LPA" ], Nights = 14
+        },
+
+        new()
+        {
+            Id = 8, Name = "Boutique Hotel Cordial La Peregrina", ArrivalDate = new DateTime(2022, 10, 10),
+            PricePerNight = 45, LocalAirports = [ "LPA" ], Nights = 7
+        },
+
+        new()
+        {
+            Id = 9, Name = "Nh Malaga", ArrivalDate = new DateTime(2023, 7, 1), PricePerNight = 83,
+            LocalAirports = [ "AGP" ], Nights = 7
+        },
+
+        new()
+        {
+            Id = 10, Name = "Barcelo Malaga", ArrivalDate = new DateTime(2023, 7, 5), PricePerNight = 45,
+            LocalAirports = [ "AGP" ], Nights = 10
+        },
+
+        new()
+        {
+            Id = 11, Name = "Parador De Malaga Gibralfaro", ArrivalDate = new DateTime(2023, 10, 16),
+            PricePerNight = 100, LocalAirports = [ "AGP" ], Nights = 7
+        },
+
+        new()
+        {
+            Id = 12, Name = "MS Maestranza Hotel", ArrivalDate = new DateTime(2023, 7, 1), PricePerNight = 45,
+            LocalAirports = [ "AGP" ], Nights = 14
+        },
+
+        new()
+        {
+            Id = 13, Name = "Jumeirah Port Soller", ArrivalDate = new DateTime(2023, 6, 15), PricePerNight = 295,
+            LocalAirports = [ "PMI" ], Nights = 10
+        }
+    ];
+}
