41b3697bf50ecf64c4b05cfbd139b51bdb6e2c55
MatthewWinterbottom <matthewwinterbottom@outlook.com>
2025-03-06 11:58:43 +0000

    RED: Any duplicate hotels in the dataset should not be returned in the results

---
+++ HoldaySearch.App/HolidaySearch.Tests/HolidaySearchDoesNotContainDuplicates.cs
@@ -0,0 +1,30 @@
+﻿using HolidaySearch.App;
+using HolidaySearch.App.Data;
+using Shouldly;
+
+namespace HolidaySearch.Tests;
+
+public class HolidaySearchDoesNotContainDuplicates
+{
+    [Test]
+    [Description("Given that the arrival date and duration are required values, we should not be getting a given hotel more than once")]
+    public void ShouldNotContainDuplicates_WhenSearchSubmitted()
+    {
+        var request = new HolidaySearchRequest
+        {
+            ArrivingAt = "PMI",
+            DepartureDate = new DateTime(2023, 06, 15),
+            Duration = 14
+        };
+        
+        var holidaySearch = new App.HolidaySearch(Flights.Data, Hotels.Data, request);
+
+        var hotels = new HashSet<string>();
+
+        foreach (var holiday in holidaySearch.Results)
+        {
+            hotels.ShouldNotContain(holiday.Hotel.Name);
+            hotels.Add(holiday.Hotel.Name);
+        }
+    }
+}
