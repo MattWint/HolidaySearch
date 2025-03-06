6eec37f4e10fc48cd64386e58770bdeadd685daa
MatthewWinterbottom <matthewwinterbottom@outlook.com>
2025-03-06 14:36:34 +0000

    RED: Duration must be greater than 0

---
+++ HoldaySearch.App/HolidaySearch.Tests/HolidaySearchPerformsValidationTests.cs
@@ -0,0 +1,24 @@
+﻿using HolidaySearch.App;
+using HolidaySearch.App.Data;
+using Shouldly;
+
+namespace HolidaySearch.Tests;
+
+public class HolidaySearchPerformsValidationTests
+{
+    [Test]
+    public void ShouldEnsureDurationIsGreaterThanZero()
+    {
+        var request = new HolidaySearchRequest
+        {
+            ArrivingAt = "LGW",
+            DepartingFrom = "LTN",
+            DepartureDate = DateTime.Today,
+            Duration = 0
+        };
+
+        Should.Throw<ArgumentException>(() =>
+            new App.HolidaySearch(Flights.Data, Hotels.Data, request)
+        );
+    }
+}
