486fc409e5ad8a6781a1b46484e87d430663a4da
MatthewWinterbottom <matthewwinterbottom@outlook.com>
2025-03-06 14:46:18 +0000

    Some further tests which also happen to be green, but give us confidence

--- HoldaySearch.App/HolidaySearch.Tests/HolidaySearchPerformsValidationTests.cs
+++ HoldaySearch.App/HolidaySearch.Tests/HolidaySearchPerformsValidationTests.cs
@@ -7,7 +7,7 @@ namespace HolidaySearch.Tests;
 public class HolidaySearchPerformsValidationTests
 {
     [Test]
-    public void ShouldEnsureDurationIsGreaterThanZero()
+    public void ShouldReturnError_WhenDurationIsGreaterThanZero()
     {
         var request = new HolidaySearchRequest
         {
@@ -20,5 +20,37 @@ public class HolidaySearchPerformsValidationTests
         Should.Throw<ArgumentException>(() =>
             new App.HolidaySearch(Flights.Data, Hotels.Data, request)
         );
+    }
+    
+    [Test]
+    public void ShouldReturnEmptyResult_WhenDepartingAirportDoesntExist()
+    {
+        var request = new HolidaySearchRequest
+        {
+            ArrivingAt = "LGW",
+            DepartingFrom = "TTT",
+            DepartureDate = DateTime.Today,
+            Duration = 10
+        };
+        
+        var holidaySearch = new App.HolidaySearch(Flights.Data, Hotels.Data, request);
+
+        holidaySearch.Results.ShouldBeEmpty();
+    }
+    
+    [Test]
+    public void ShouldReturnEmptyResult_WhenArrivingAirportDoesntExist()
+    {
+        var request = new HolidaySearchRequest
+        {
+            ArrivingAt = "TTT",
+            DepartingFrom = "PMI",
+            DepartureDate = DateTime.Today,
+            Duration = 10
+        };
+        
+        var holidaySearch = new App.HolidaySearch(Flights.Data, Hotels.Data, request);
+
+        holidaySearch.Results.ShouldBeEmpty();
     }
 }
