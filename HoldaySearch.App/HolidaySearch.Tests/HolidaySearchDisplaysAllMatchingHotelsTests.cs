1b7a59cd9e3565df57c79dc47ebff7feb0c21baf
MatthewWinterbottom <matthewwinterbottom@outlook.com>
2025-03-06 09:48:31 +0000

    RED: Added a test to ensure we are returning all of the valid options. E.g. if there's two flights and two hotels, we should return four results. Yes, this contains duplicate hotels, but the UI can have logic to ensure each hotel is displayed once, and then give an option of flights.

---
+++ HoldaySearch.App/HolidaySearch.Tests/HolidaySearchDisplaysAllMatchingHotelsTests.cs
@@ -0,0 +1,42 @@
+﻿using HolidaySearch.App;
+using Shouldly;
+
+namespace HolidaySearch.Tests;
+
+public class HolidaySearchDisplaysAllMatchingHotelsTests
+{
+    [Test]
+    [TestCaseSource(nameof(HotelPairsTestCases))]
+    [Description("Ensure each hotel is paired with each flight. E.g. if there's two hotels, pair with two flights means 4 results." +
+                 "The consumer (UI) could then display each hotel once with the ability to edit the flight")]
+    public void ShouldMatchAllHotelsWithFlights_WhenMultipleHotelsMatchCriteria(HolidaySearchRequest request, (int flightId, int hotelId)[] combinations)
+    {
+        var holidaySearch = new App.HolidaySearch(request);
+        
+        holidaySearch.Results.Count.ShouldBe(combinations.Length);
+
+        for (var i = 0; i < holidaySearch.Results.Count; i++)
+        {
+            var searchResult = holidaySearch.Results[i];
+            var expectedResult = combinations[i];
+            
+            searchResult.Flight.Id.ShouldBe(expectedResult.flightId);
+            searchResult.Hotel.Id.ShouldBe(expectedResult.hotelId);
+        }
+    }
+    
+    private static IEnumerable<TestCaseData> HotelPairsTestCases()
+    {
+        yield return new TestCaseData(
+            new HolidaySearchRequest
+                { DepartingFrom = "MAN", ArrivingAt = "PMI", DepartureDate = new DateTime(2023, 6, 15), Duration = 10 },
+            new (int flightId, int hotelId)[]
+            {
+                (5, 5),
+                (5, 13),
+                (3, 5),
+                (3, 13)
+            }
+        ).SetName("Manchester to Mallorca");
+    }
+}
